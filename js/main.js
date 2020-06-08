f511cf5ae501eab1a394f3ea74569ebd4259b621
ikonastilya <eimantanaswerke@gmail.com>
2020-06-08 17:54:03 +0300

    the entire site

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".nav_link").hover(function(){
+      $(this).css("color", "black");
+      }, function(){
+      $(this).css("color", "white");
+    });
+  });
+
+  $(document).ready(function(){
+    $(".btn").hover(function(){
+      $(this).css("background-color", "black");
+      }, function(){
+      $(this).css("background-color", "#008000");
+    });
+  });
+
+  $(document).ready(function(){
+    $(".btn").hover(function(){
+      $(this).css("color", "green");
+      }, function(){
+      $(this).css("color", "white");
+    });
+  });
